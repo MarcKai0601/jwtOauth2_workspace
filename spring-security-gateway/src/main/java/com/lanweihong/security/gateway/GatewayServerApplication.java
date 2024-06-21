2a497de91118afaa1b5bc79c34c3c405f9710c32
zheng.kai <zheng.kai@jyintgroup.com>
2024-06-21 18:08:32 +0800

    gateway 配置練習

--- spring-security-gateway/src/main/java/com/lanweihong/security/gateway/GatewayServerApplication.java
+++ spring-security-gateway/src/main/java/com/lanweihong/security/gateway/GatewayServerApplication.java
@@ -1,5 +1,8 @@
 package com.lanweihong.security.gateway;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class GatewayServerApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
