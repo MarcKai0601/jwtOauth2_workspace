1a9b74465923b720d8bb1251a53b14c3aadeef41
zheng.kai <zheng.kai@jyintgroup.com>
2024-06-21 11:01:00 +0800

    nacos 配置成功

--- spring-security-auth/src/main/java/com/lanweihong/security/auth/AuthServerApplication.java
+++ spring-security-auth/src/main/java/com/lanweihong/security/auth/AuthServerApplication.java
@@ -5,6 +5,7 @@ import com.lanweihong.security.auth.config.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -17,6 +18,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableCaching
 @EnableResourceServer
 @EnableAuthorizationServer
+@EnableDiscoveryClient
 public class AuthServerApplication {
 
     public static void main(String[] args) {
