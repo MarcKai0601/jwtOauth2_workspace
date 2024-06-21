9f2fc68b9e96a87795d95850d4d355a059d77ccc
zheng.kai <zheng.kai@jyintgroup.com>
2024-06-21 11:01:18 +0800

    配置gateway

---
+++ spring-security-gateway/src/main/java/com/lanweihong/security/gateway/GatewayServerApplication.java
@@ -0,0 +1,7 @@
+package com.lanweihong.security.gateway;
+
+public class GatewayServerApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
